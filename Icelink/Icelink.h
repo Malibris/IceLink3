286736704bf49db3bae74c64bf363505dceb2bcb
Robert Abramczyk <robert.abramczyk@netguru.pl>
2018-04-20 08:58:20 +0200

    Cleanup

--- Icelink/Icelink.h
+++ Icelink/Icelink.h
@@ -2,27 +2,11 @@
 //  Icelink.h
 //  Icelink
 //
-//  Copyright © 2017 Netguru Sp. z o.o. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for Icelink.
 FOUNDATION_EXPORT double IcelinkVersionNumber;
-
-//! Project version string for Icelink.
 FOUNDATION_EXPORT const unsigned char IcelinkVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Icelink/PublicHeader.h>
-
-/*#import <Icelink/FM.h>
-#import <Icelink/FMWebSync.h>
-#import <Icelink/FMWebSyncSubscribers.h>
-#import <Icelink/FMWebSyncChat.h>
-#import <Icelink/FMIceLink.h>
-#import <Icelink/FMIceLinkSimple.h>
-#import <Icelink/FMIceLinkWebRTC.h>
-#import <Icelink/FMIceLinkWebSync.h>*/
 
 #import "FMIceLink.swift3.h"
 #import "FMIceLinkCocoa.swift3.h"
