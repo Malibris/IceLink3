908f25fdb98945eba66c8a3ccf9a8e2241cdd9d9
Robert Abramczyk <robert.abramczyk@netguru.pl>
2018-05-23 10:09:37 +0200

    Rearrange headers

--- Icelink/Icelink.h
+++ Icelink/Icelink.h
@@ -8,10 +8,10 @@
 FOUNDATION_EXPORT double IcelinkVersionNumber;
 FOUNDATION_EXPORT const unsigned char IcelinkVersionString[];
 
+#import <Icelink/FMIceLink.swift3.h>
 #import <Icelink/FMIceLinkCocoa.swift3.h>
-#import <Icelink/FMWebSyncChat-Swift3.h>
-#import <Icelink/FMWebSyncSubscribers-Swift3.h>
 #import <Icelink/FMWebSync-Swift3.h>
+#import <Icelink/FMWebSyncSubscribers-Swift3.h>
 #import <Icelink/FMIceLinkWebSync4.swift3.h>
 
 #import <Icelink/FMIceLinkYuv.swift3.h>
